6078c2a955be8ee1b8c7ddc97e87ca320b1010a8
Michael Hermes <mikehermes87@gmail.com>
2021-08-24 09:08:15 -0700

    Add request validation when creating a new note via POST.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
+// A helper function to write a set of notes to the 'db.json' datastore.
 const writeFile = data => {
 	fs.writeFile(dbFilename, data, err => {
 		if (err) throw err;
@@ -18,19 +19,35 @@ const writeFile = data => {
 	});
 };
 
+// Displays the 'notes.html' page in the browser.
 app.get("/notes", (req, res) =>
 	res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+// Returns all notes to the client in JSON format.
 app.get("/api/notes", (req, res) => {
 	fs.readFile(dbFilename, (err, data) => {
 		if (err) throw err;
-		res.json(JSON.parse(data));
+		if (data.length > 0) {
+			return res.json(JSON.parse(data));
+		}
+		return res.json([]);
 	});
 });
 
+// Adds a new note to the list of notes and returns the newly added note to the client in JSON format. Returns a 400 Bad Request if any required fields are missing from the request.
 app.post("/api/notes", (req, res) => {
 	// Validate the request body
+	const schema = Joi.object({
+		title: Joi.string().required(),
+		text: Joi.string().required(),
+	});
+	const { error } = schema.validate(req.body, { abortEarly: false });
+
+	// Return a 400 Bad Request if we're missing required fields and stop processing the POST
+	if (error) {
+		return res.status(400).json({ error: error.details.map(e => e.message) });
+	}
 
 	// Create a new note object
 	const note = {
@@ -43,19 +60,24 @@ app.post("/api/notes", (req, res) => {
 	fs.readFile(dbFilename, (err, data) => {
 		// Handle any error
 		if (err) throw err;
-		// Read the current notes from the datastore
-		let notes = JSON.parse(data);
-		// Add a new note to the datastore
+		// Attempt to parse any existing notes
+		let notes = [];
+		if (data.length > 0) {
+			notes = JSON.parse(data);
+		}
+		// Add the new note to the datastore and return the new note to the client
 		notes.push(note);
-		// Write the updated notes object to the datastore file.
 		writeFile(JSON.stringify(notes));
+		res.json(note);
 	});
 });
 
+// Default route handler to display the 'index.html' page in the browser.
 app.get("*", (req, res) =>
 	res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
+// Start our express server listening on the given PORT.
 app.listen(PORT, () => {
 	console.log(`Server is listenting at http://localhost:${PORT}...`);
 });
