4ad889998a692a8c69b95edcb14e133a353f4e78
Michael Hermes <mikehermes87@gmail.com>
2021-08-23 17:23:03 -0700

    GET '/api/notes/' is returning JSON data.

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get("/notes", (req, res) =>
 	res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+app.get("/api/notes", (req, res) => res.json(db));
+
+app.post("/api/notes", (req, res) => {});
+
 app.get("*", (req, res) =>
 	res.sendFile(path.join(__dirname, "public/index.html"))
 );
